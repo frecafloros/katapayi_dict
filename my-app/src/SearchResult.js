988425180aa19cc95a34660e2f8921ce40cd170e
frecafloros <frecafloros@gmail.com>
2024-03-02 21:33:49 +0900

    ver.5.0

--- my-app/src/SearchResult.js
+++ my-app/src/SearchResult.js
@@ -26,7 +26,7 @@ const SearchResult = ({ searchResults, searchTerm }) => {
             <ul>
               {sortedResults.map((word, index) => (
                 <li key={index}>
-                  <span className="kaei-maru">{word.entry.form}</span>&nbsp;<br />
+                  <span className="kaei-rom">{word.entry.form}</span>&nbsp;<br />
                   <span style={{ color: 'gray' }}>{word.entry.form}</span>&nbsp;
                   <span style={{ color: 'gray', fontFamily: 'Times New Roman' }}>/{word.contents.find((item) => item.title === "Pronunciation")?.text}/</span><br />
                   [{word.translations[0]?.title}]&nbsp;
